2de4fc4061abe2493dd188b689cc2554a6162fb0
hr-shuvo <h.r.shuvo9@gmail.com>
2024-12-06 07:45:34 +0600

    using mlflow

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
+import mlflow
+import mlflow.sklearn
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
 # get arguments from command
 parser = argparse.ArgumentParser()
-parser.add_argument("--alpha", type=float, default=0.5, required=False)
+parser.add_argument("--alpha", type=float, default=0.7, required=False)
 parser.add_argument("--l1_ratio", type=float, default=0.5, required=False)
 args = parser.parse_args()
 
@@ -47,18 +49,28 @@ if __name__ == "__main__":
 
     alpha = args.alpha
     l1_ratio = args.l1_ratio
+    exp = mlflow.set_experiment(experiment_name="experiment_1")
 
-    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-    lr.fit(train_x, train_y)
+    with mlflow.start_run(experiment_id=exp.experiment_id) as run:
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
 
-    predicted_qualities = lr.predict(test_x)
+        predicted_qualities = lr.predict(test_x)
 
-    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-    print(f"RMSE: {rmse}")
-    print(f"MAE: {mae}")
-    print(f"R2: {r2}")
-    print("ElasticNet model (alpha={:f}, l1_ratio={:f})".format(alpha, l1_ratio))
+        print(f"RMSE: {rmse}")
+        print(f"MAE : {mae}")
+        print(f"R2  : {r2}")
+        print("ElasticNet model (alpha={:f}, l1_ratio={:f})".format(alpha, l1_ratio))
+
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2", r2)
+
+        mlflow.sklearn.log_model(lr, "mymodel")
 
 
 

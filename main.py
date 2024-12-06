6bf6259679f29f22180cb8ffc79f368e8fe6b236
hr-shuvo <h.r.shuvo9@gmail.com>
2024-12-06 07:21:45 +0600

    add simple prediction

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
+import warnings
+import argparse
+import logging
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+# get arguments from command
+parser = argparse.ArgumentParser()
+parser.add_argument("--alpha", type=float, default=0.5, required=False)
+parser.add_argument("--l1_ratio", type=float, default=0.5, required=False)
+args = parser.parse_args()
+
+# evaluation function
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+
+    # Read the wine-quality csv file from the url
+    csv_url = (
+        "data/red-wine-quality.csv"
+    )
+
+    data = pd.read_csv(csv_url)
+
+    # Split the data into training and test sets. (0.75, 0.25)
+    train, test = train_test_split(data, random_state=42)
+
+    # The predicted column is "quality" which is a scaler from [3, 9]
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train["quality"]
+    test_y = test["quality"]
+
+    alpha = args.alpha
+    l1_ratio = args.l1_ratio
+
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print(f"RMSE: {rmse}")
+    print(f"MAE: {mae}")
+    print(f"R2: {r2}")
+    print("ElasticNet model (alpha={:f}, l1_ratio={:f})".format(alpha, l1_ratio))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+

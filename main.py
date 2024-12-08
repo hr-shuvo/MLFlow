49870d97a27cd0141cc7f67918a0411fd6eb466a
hr-shuvo <h.r.shuvo9@gmail.com>
2024-12-09 03:41:34 +0600

    add custom model

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ import logging
 
 import pandas as pd
 import numpy as np
+from mlflow import sklearn
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 import mlflow
 import mlflow.sklearn
+
 from pathlib import Path
 import os
 from mlflow.models.signature import ModelSignature, infer_signature
 from mlflow.types.schema import Schema, ColSpec
+import sklearn
+import joblib
+import cloudpickle
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -47,8 +52,16 @@ if __name__ == "__main__":
 
     # Split the data into training and test sets. (0.75, 0.25)
     train, test = train_test_split(data, random_state=42)
-    train.to_csv("data/train.csv", index=False)
-    test.to_csv("data/test.csv", index=False)
+    # train.to_csv("data/train.csv", index=False)
+    # test.to_csv("data/test.csv", index=False)
+
+    data_dir = 'red-wine_data'
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+
+    data.to_csv(data_dir + "/data.csv", index=False)
+    train.to_csv(data_dir + "/train.csv", index=False)
+    test.to_csv(data_dir + "/test.csv", index=False)
 
     # The predicted column is "quality" which is a scaler from [3, 9]
     train_x = train.drop(["quality"], axis=1)
@@ -65,7 +78,7 @@ if __name__ == "__main__":
     print("The set tracking uri is ", mlflow.get_tracking_uri())
 
     exp= mlflow.set_experiment(
-        experiment_name="experiment_signature"
+        experiment_name="experiment_custom_sklearn"
     )
     get_exp = mlflow.get_experiment(exp.experiment_id)
 
@@ -106,16 +119,69 @@ if __name__ == "__main__":
     print(f"R2  : {r2}")
     print("ElasticNet model (alpha={:f}, l1_ratio={:f})".format(alpha, l1_ratio))
 
+    mlflow.log_params({
+        'alpha': alpha,
+        'l1_ratio': l1_ratio
+    })
 
-    signature = infer_signature(test_x, predicted_qualities)
-    input_example = {
-        "columns": np.array(test_x.columns),
-        "data": np.array(test_x.values)
+    mlflow.log_metrics({
+        'rmse': rmse,
+        'mae': mae,
+        'r2': r2
+    })
+
+    sklearn_model_path = "sklearn_model.pkl"
+    joblib.dump(lr, sklearn_model_path)
+    artifacts = {
+        'sklearn_model': sklearn_model_path,
+        'data': data_dir
     }
 
 
-    mlflow.log_artifact('red-wine-quality.csv')
-    mlflow.sklearn.log_model(lr, "model", signature=signature, input_example=input_example)
+    class SklearnWrapper(mlflow.pyfunc.PythonModel):
+        # def __init__(self):
+        #     self.sklearn_model = None
+
+        def load_context(self, context):
+            self.sklearn_model = joblib.load(context.artifacts['sklearn_model'])
+
+        def predict(self, context, model_input):
+            return self.sklearn_model.predict(model_input.values)
+
+    conda_env = {
+        "channels": ["defaults"],
+        "dependencies": [
+            "python={}".format(3.10),
+            "pip",
+            {
+                "pip": [
+                    "mlflow=={}".format(mlflow.__version__),
+                    "scikit-learn=={}".format(sklearn.__version__),
+                    "cloudpickle=={}".format(cloudpickle.__version__),
+                ],
+            },
+        ],
+        "name": "sklearn_env",
+    }
+
+
+
+    mlflow.pyfunc.log_model(
+        artifact_path="sklear_mlflow_pyfunc",
+        python_model=SklearnWrapper(),
+        artifacts=artifacts,
+        code_path=['main.py'],
+        conda_env=conda_env,
+
+    )
+
+    ld = mlflow.pyfunc.load_model(model_uri='runs:/5ec61b4e47c44990bbff990e011d185f/sklear_mlflow_pyfunc')
+    predicted_qualities = ld.predict(test_x)
+
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    print(f"RMSE: {rmse}")
+    print(f"MAE : {mae}")
+    print(f"R2  : {r2}")
 
     artifact_uri = mlflow.get_artifact_uri()
     print("The artifact path is ", artifact_uri)
